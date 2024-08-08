7edf69cc699489bdf4c0a88f698c68213473d943
philhassey <phil@strongdm.com>
2024-08-08 16:54:16 -0600

    ast: move x/exp/ast to ast
    Addresses IDX-142
    
    Signed-off-by: philhassey <phil@strongdm.com>

--- ast/cedar_parse_test.go
+++ ast/cedar_parse_test.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/cedar-policy/cedar-go/ast"
 	"github.com/cedar-policy/cedar-go/internal/testutil"
-	"github.com/cedar-policy/cedar-go/x/exp/ast"
 )
 
 func TestParse(t *testing.T) {
--- ast/json_test.go
+++ ast/json_test.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/cedar-policy/cedar-go/ast"
 	"github.com/cedar-policy/cedar-go/internal/testutil"
 	"github.com/cedar-policy/cedar-go/types"
-	"github.com/cedar-policy/cedar-go/x/exp/ast"
 )
 
 func TestUnmarshalJSON(t *testing.T) {
