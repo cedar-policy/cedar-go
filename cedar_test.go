8bd3ecc18b03f224531098802c504811d2c9bc68
Patrick Jakubowski <patrick.jakubowski@strongdm.com>
2024-08-13 17:25:37 -0700

    cedar-go/internal/entities: move TestEntitiesJSON to entities package
    Signed-off-by: philhassey <phil@strongdm.com>

--- cedar_test.go
+++ cedar_test.go
@@ -1,10 +1,8 @@
 package cedar
 
 import (
-	"encoding/json"
 	"testing"
 
-	"github.com/cedar-policy/cedar-go/internal/entities"
 	"github.com/cedar-policy/cedar-go/internal/testutil"
 	"github.com/cedar-policy/cedar-go/types"
 )
@@ -47,45 +45,5 @@ func TestNewPolicySet(t *testing.T) {
 		ps, err := NewPolicySet("policy.cedar", []byte(`@key("value") permit (principal, action, resource);`))
 		testutil.OK(t, err)
 		testutil.Equals(t, ps[0].Annotations, Annotations{"key": "value"})
-	})
-}
-
-func TestEntitiesJSON(t *testing.T) {
-	t.Parallel()
-	t.Run("Marshal", func(t *testing.T) {
-		t.Parallel()
-		e := entities.Entities{}
-		ent := entities.Entity{
-			UID:        types.NewEntityUID("Type", "id"),
-			Parents:    []types.EntityUID{},
-			Attributes: types.Record{"key": types.Long(42)},
-		}
-		e[ent.UID] = ent
-		b, err := e.MarshalJSON()
-		testutil.OK(t, err)
-		testutil.Equals(t, string(b), `[{"uid":{"type":"Type","id":"id"},"attrs":{"key":42}}]`)
-	})
-
-	t.Run("Unmarshal", func(t *testing.T) {
-		t.Parallel()
-		b := []byte(`[{"uid":{"type":"Type","id":"id"},"parents":[],"attrs":{"key":42}}]`)
-		var e entities.Entities
-		err := json.Unmarshal(b, &e)
-		testutil.OK(t, err)
-		want := entities.Entities{}
-		ent := entities.Entity{
-			UID:        types.NewEntityUID("Type", "id"),
-			Parents:    []types.EntityUID{},
-			Attributes: types.Record{"key": types.Long(42)},
-		}
-		want[ent.UID] = ent
-		testutil.Equals(t, e, want)
-	})
-
-	t.Run("UnmarshalErr", func(t *testing.T) {
-		t.Parallel()
-		var e entities.Entities
-		err := e.UnmarshalJSON([]byte(`!@#$`))
-		testutil.Error(t, err)
 	})
 }
--- internal/entities/entities_test.go
+++ internal/entities/entities_test.go
@@ -1,6 +1,7 @@
 package entities
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/cedar-policy/cedar-go/internal/testutil"
@@ -21,4 +22,44 @@ func TestEntities(t *testing.T) {
 		testutil.Equals(t, clone, e)
 	})
 
+}
+
+func TestEntitiesJSON(t *testing.T) {
+	t.Parallel()
+	t.Run("Marshal", func(t *testing.T) {
+		t.Parallel()
+		e := Entities{}
+		ent := Entity{
+			UID:        types.NewEntityUID("Type", "id"),
+			Parents:    []types.EntityUID{},
+			Attributes: types.Record{"key": types.Long(42)},
+		}
+		e[ent.UID] = ent
+		b, err := e.MarshalJSON()
+		testutil.OK(t, err)
+		testutil.Equals(t, string(b), `[{"uid":{"type":"Type","id":"id"},"attrs":{"key":42}}]`)
+	})
+
+	t.Run("Unmarshal", func(t *testing.T) {
+		t.Parallel()
+		b := []byte(`[{"uid":{"type":"Type","id":"id"},"parents":[],"attrs":{"key":42}}]`)
+		var e Entities
+		err := json.Unmarshal(b, &e)
+		testutil.OK(t, err)
+		want := Entities{}
+		ent := Entity{
+			UID:        types.NewEntityUID("Type", "id"),
+			Parents:    []types.EntityUID{},
+			Attributes: types.Record{"key": types.Long(42)},
+		}
+		want[ent.UID] = ent
+		testutil.Equals(t, e, want)
+	})
+
+	t.Run("UnmarshalErr", func(t *testing.T) {
+		t.Parallel()
+		var e Entities
+		err := e.UnmarshalJSON([]byte(`!@#$`))
+		testutil.Error(t, err)
+	})
 }
