1f7e55125245d6e59454441509d20ce1ecfed6f8
Patrick Jakubowski <patrick.jakubowski@strongdm.com>
2024-08-14 15:18:22 -0700

    cedar-go: add a way to create a PolicySet from a set of existing Policys

--- policy_set_test.go
+++ policy_set_test.go
@@ -3,6 +3,7 @@ package cedar
 import (
 	"testing"
 
+	"github.com/cedar-policy/cedar-go/ast"
 	"github.com/cedar-policy/cedar-go/internal/testutil"
 )
 
@@ -23,5 +24,33 @@ func TestNewPolicySet(t *testing.T) {
 		ps, err := NewPolicySet("policy.cedar", []byte(`@key("value") permit (principal, action, resource);`))
 		testutil.OK(t, err)
 		testutil.Equals(t, ps.GetPolicy("policy0").Annotations, Annotations{"key": "value"})
+	})
+}
+
+func TestNewPolicySetFromPolicies(t *testing.T) {
+	t.Parallel()
+	t.Run("empty slice", func(t *testing.T) {
+		t.Parallel()
+
+		var policies []*Policy
+		ps := NewPolicySetFromPolicies(policies)
+
+		testutil.Equals(t, ps.GetPolicy("policy0"), nil)
+	})
+	t.Run("non-empty slice", func(t *testing.T) {
+		t.Parallel()
+
+		policy0 := NewPolicyFromAST(ast.Forbid())
+
+		var policy1 Policy
+		testutil.OK(t, policy1.UnmarshalJSON(
+			[]byte(`{"effect":"permit","principal":{"op":"All"},"action":{"op":"All"},"resource":{"op":"All"}}`),
+		))
+
+		ps := NewPolicySetFromPolicies([]*Policy{policy0, &policy1})
+
+		testutil.Equals(t, ps.GetPolicy("policy0"), policy0)
+		testutil.Equals(t, ps.GetPolicy("policy1"), &policy1)
+		testutil.Equals(t, ps.GetPolicy("policy2"), nil)
 	})
 }
