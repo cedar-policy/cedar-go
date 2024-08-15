bc2cf7f31d85c17028e5509b1821fcbc00d4a87f
philhassey <phil@strongdm.com>
2024-08-15 14:27:41 -0600

    internal/eval: add test coverage for toEval
    Addresses IDX-142
    
    Signed-off-by: philhassey <phil@strongdm.com>

--- internal/eval/convert.go
+++ internal/eval/convert.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/cedar-policy/cedar-go/internal/ast"
 	"github.com/cedar-policy/cedar-go/internal/extensions"
-	"github.com/cedar-policy/cedar-go/types"
 )
 
 func toEval(n ast.IsNode) Evaler {
@@ -26,41 +25,38 @@ func toEval(n ast.IsNode) Evaler {
 		rhs := newInEval(obj, toEval(v.Entity))
 		return newAndEval(lhs, rhs)
 	case ast.NodeTypeExtensionCall:
-		i, ok := extensions.ExtMap[v.Name]
-		if !ok {
-			return newErrorEval(fmt.Errorf("%w: %s", errUnknownExtensionFunction, v.Name))
-		}
-		if i.Args != len(v.Args) {
-			return newErrorEval(fmt.Errorf("%w: %s takes %d parameter(s)", errArity, v.Name, i.Args))
-		}
-		switch {
-		case v.Name == "ip":
-			return newIPLiteralEval(toEval(v.Args[0]))
-		case v.Name == "decimal":
-			return newDecimalLiteralEval(toEval(v.Args[0]))
+		if i, ok := extensions.ExtMap[v.Name]; ok {
+			if i.Args != len(v.Args) {
+				return newErrorEval(fmt.Errorf("%w: %s takes %d parameter(s)", errArity, v.Name, i.Args))
+			}
+			switch {
+			case v.Name == "ip":
+				return newIPLiteralEval(toEval(v.Args[0]))
+			case v.Name == "decimal":
+				return newDecimalLiteralEval(toEval(v.Args[0]))
 
-		case v.Name == "lessThan":
-			return newDecimalLessThanEval(toEval(v.Args[0]), toEval(v.Args[1]))
-		case v.Name == "lessThanOrEqual":
-			return newDecimalLessThanOrEqualEval(toEval(v.Args[0]), toEval(v.Args[1]))
-		case v.Name == "greaterThan":
-			return newDecimalGreaterThanEval(toEval(v.Args[0]), toEval(v.Args[1]))
-		case v.Name == "greaterThanOrEqual":
-			return newDecimalGreaterThanOrEqualEval(toEval(v.Args[0]), toEval(v.Args[1]))
+			case v.Name == "lessThan":
+				return newDecimalLessThanEval(toEval(v.Args[0]), toEval(v.Args[1]))
+			case v.Name == "lessThanOrEqual":
+				return newDecimalLessThanOrEqualEval(toEval(v.Args[0]), toEval(v.Args[1]))
+			case v.Name == "greaterThan":
+				return newDecimalGreaterThanEval(toEval(v.Args[0]), toEval(v.Args[1]))
+			case v.Name == "greaterThanOrEqual":
+				return newDecimalGreaterThanOrEqualEval(toEval(v.Args[0]), toEval(v.Args[1]))
 
-		case v.Name == "isIpv4":
-			return newIPTestEval(toEval(v.Args[0]), ipTestIPv4)
-		case v.Name == "isIpv6":
-			return newIPTestEval(toEval(v.Args[0]), ipTestIPv6)
-		case v.Name == "isLoopback":
-			return newIPTestEval(toEval(v.Args[0]), ipTestLoopback)
-		case v.Name == "isMulticast":
-			return newIPTestEval(toEval(v.Args[0]), ipTestMulticast)
-		case v.Name == "isInRange":
-			return newIPIsInRangeEval(toEval(v.Args[0]), toEval(v.Args[1]))
-		default:
-			panic(fmt.Errorf("unknown extension: %v", v.Name))
+			case v.Name == "isIpv4":
+				return newIPTestEval(toEval(v.Args[0]), ipTestIPv4)
+			case v.Name == "isIpv6":
+				return newIPTestEval(toEval(v.Args[0]), ipTestIPv6)
+			case v.Name == "isLoopback":
+				return newIPTestEval(toEval(v.Args[0]), ipTestLoopback)
+			case v.Name == "isMulticast":
+				return newIPTestEval(toEval(v.Args[0]), ipTestMulticast)
+			case v.Name == "isInRange":
+				return newIPIsInRangeEval(toEval(v.Args[0]), toEval(v.Args[1]))
+			}
 		}
+		return newErrorEval(fmt.Errorf("%w: %s", errUnknownExtensionFunction, v.Name))
 	case ast.NodeValue:
 		return newLiteralEval(v.Value)
 	case ast.NodeTypeRecord:
@@ -96,8 +92,12 @@ func toEval(n ast.IsNode) Evaler {
 		return newInEval(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeAnd:
 		return newAndEval(toEval(v.Left), toEval(v.Right))
+	case ast.NodeTypeOr:
+		return newOrNode(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeEquals:
 		return newEqualEval(toEval(v.Left), toEval(v.Right))
+	case ast.NodeTypeNotEquals:
+		return newNotEqualEval(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeGreaterThan:
 		return newLongGreaterThanEval(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeGreaterThanOrEqual:
@@ -110,43 +110,15 @@ func toEval(n ast.IsNode) Evaler {
 		return newSubtractEval(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeAdd:
 		return newAddEval(toEval(v.Left), toEval(v.Right))
+	case ast.NodeTypeMult:
+		return newMultiplyEval(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeContains:
 		return newContainsEval(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeContainsAll:
 		return newContainsAllEval(toEval(v.Left), toEval(v.Right))
 	case ast.NodeTypeContainsAny:
 		return newContainsAnyEval(toEval(v.Left), toEval(v.Right))
-	case ast.NodeTypeMult:
-		return newMultiplyEval(toEval(v.Left), toEval(v.Right))
-	case ast.NodeTypeNotEquals:
-		return newNotEqualEval(toEval(v.Left), toEval(v.Right))
-	case ast.NodeTypeOr:
-		return newOrNode(toEval(v.Left), toEval(v.Right))
 	default:
 		panic(fmt.Sprintf("unknown node type %T", v))
-	}
-}
-
-func scopeToNode(varNode ast.NodeTypeVariable, in ast.IsScopeNode) ast.Node {
-	switch t := in.(type) {
-	case ast.ScopeTypeAll:
-		return ast.True()
-	case ast.ScopeTypeEq:
-		return ast.NewNode(varNode).Equals(ast.EntityUID(t.Entity))
-	case ast.ScopeTypeIn:
-		return ast.NewNode(varNode).In(ast.EntityUID(t.Entity))
-	case ast.ScopeTypeInSet:
-		set := make([]types.Value, len(t.Entities))
-		for i, e := range t.Entities {
-			set[i] = e
-		}
-		return ast.NewNode(varNode).In(ast.Set(set))
-	case ast.ScopeTypeIs:
-		return ast.NewNode(varNode).Is(t.Type)
-
-	case ast.ScopeTypeIsIn:
-		return ast.NewNode(varNode).IsIn(t.Type, ast.EntityUID(t.Entity))
-	default:
-		panic(fmt.Sprintf("unknown scope type %T", t))
 	}
 }
