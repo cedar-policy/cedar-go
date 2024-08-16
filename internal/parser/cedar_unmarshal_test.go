fb91513bcc154c9176fc3866bd34c45dc9df47bd
Patrick Jakubowski <patrick.jakubowski@strongdm.com>
2024-08-16 11:36:34 -0700

    cedar-go/types: rename ParsePattern to UnmarshalCedar and follow the typical Unmarshaling interface shape
    Signed-off-by: philhassey <phil@strongdm.com>

--- internal/parser/cedar_unmarshal_test.go
+++ internal/parser/cedar_unmarshal_test.go
@@ -288,19 +288,19 @@ when { principal has "1stName" };`,
 			"like no wildcards",
 			`permit ( principal, action, resource )
 when { principal.firstName like "johnny" };`,
-			ast.Permit().When(ast.Principal().Access("firstName").Like(testutil.Must(types.ParsePattern("johnny")))),
+			ast.Permit().When(ast.Principal().Access("firstName").Like(types.Pattern{}.Literal("johnny"))),
 		},
 		{
 			"like escaped asterisk",
 			`permit ( principal, action, resource )
 when { principal.firstName like "joh\*nny" };`,
-			ast.Permit().When(ast.Principal().Access("firstName").Like(testutil.Must(types.ParsePattern(`joh\*nny`)))),
+			ast.Permit().When(ast.Principal().Access("firstName").Like(types.Pattern{}.Literal("joh*nny"))),
 		},
 		{
 			"like wildcard",
 			`permit ( principal, action, resource )
 when { principal.firstName like "*" };`,
-			ast.Permit().When(ast.Principal().Access("firstName").Like(testutil.Must(types.ParsePattern("*")))),
+			ast.Permit().When(ast.Principal().Access("firstName").Like(types.Pattern{}.Wildcard())),
 		},
 		{
 			"is",
