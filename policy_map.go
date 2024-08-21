58767268303af6ff61b771f431874597351be67f
philhassey <phil@strongdm.com>
2024-08-21 11:41:38 -0600

    cedar: add Map method to PolicySet
    Signed-off-by: philhassey <phil@strongdm.com>

--- policy_map.go
+++ policy_map.go
@@ -4,22 +4,25 @@ package cedar
 import (
 	"bytes"
 	"fmt"
+	"maps"
 	"slices"
 )
 
 // PolicyID is a string identifier for the policy within the PolicySet
 type PolicyID string
 
-type policyMap map[PolicyID]Policy
+// PolicyMap is a map of policy IDs to policy
+type PolicyMap map[PolicyID]Policy
 
 // PolicySet is a set of named policies against which a request can be authorized.
 type PolicySet struct {
-	policies policyMap
+	// policies are stored internally so we can handle performance, concurrency bookkeeping however we want
+	policies PolicyMap
 }
 
 // NewPolicySet creates a new, empty PolicySet
 func NewPolicySet() PolicySet {
-	return PolicySet{policies: policyMap{}}
+	return PolicySet{policies: PolicyMap{}}
 }
 
 // NewPolicySetFromBytes will create a PolicySet from the given text document with the given file name used in Position
@@ -32,7 +35,7 @@ func NewPolicySetFromBytes(fileName string, document []byte) (PolicySet, error) 
 	if err != nil {
 		return PolicySet{}, err
 	}
-	policyMap := make(policyMap, len(policySlice))
+	policyMap := make(PolicyMap, len(policySlice))
 	for i, p := range policySlice {
 		policyID := PolicyID(fmt.Sprintf("policy%d", i))
 		policyMap[policyID] = p
@@ -40,13 +43,13 @@ func NewPolicySetFromBytes(fileName string, document []byte) (PolicySet, error) 
 	return PolicySet{policies: policyMap}, nil
 }
 
-// Get returns a pointer to the Policy with the given ID. If a policy with the given ID does not exist, an empty policy is returned.
+// Get returns the Policy with the given ID. If a policy with the given ID does not exist, an empty policy is returned.
 func (p PolicySet) Get(policyID PolicyID) Policy {
 	return p.policies[policyID]
 }
 
-// Upsert inserts or updates a policy with the given ID.
-func (p *PolicySet) Upsert(policyID PolicyID, policy Policy) {
+// Set inserts or updates a policy with the given ID.
+func (p *PolicySet) Set(policyID PolicyID, policy Policy) {
 	p.policies[policyID] = policy
 }
 
@@ -55,13 +58,10 @@ func (p *PolicySet) Delete(policyID PolicyID) {
 	delete(p.policies, policyID)
 }
 
-// // UpsertPolicySet inserts or updates all the policies from src into this PolicySet. Policies in this PolicySet with
-// // identical IDs in src are clobbered by the policies from src.
-// func (p *PolicySet) UpsertPolicySet(src PolicySet) {
-// 	for id, policy := range src.policies {
-// 		p.policies[id] = policy
-// 	}
-// }
+// Map returns a new PolicyMap instance of the policies in the PolicySet.
+func (p *PolicySet) Map() PolicyMap {
+	return maps.Clone(p.policies)
+}
 
 // MarshalCedar emits a concatenated Cedar representation of a PolicySet. The policy names are stripped, but policies
 // are emitted in lexicographical order by ID.
--- policy_map_test.go
+++ policy_map_test.go
@@ -42,8 +42,8 @@ func TestUpsertPolicy(t *testing.T) {
 		))
 
 		ps := cedar.NewPolicySet()
-		ps.Upsert("policy0", policy0)
-		ps.Upsert("policy1", policy1)
+		ps.Set("policy0", policy0)
+		ps.Set("policy1", policy1)
 
 		testutil.Equals(t, ps.Get("policy0"), policy0)
 		testutil.Equals(t, ps.Get("policy1"), policy1)
@@ -55,68 +55,14 @@ func TestUpsertPolicy(t *testing.T) {
 		ps := cedar.NewPolicySet()
 
 		p1 := cedar.NewPolicyFromAST(ast.Forbid())
-		ps.Upsert("a wavering policy", p1)
+		ps.Set("a wavering policy", p1)
 
 		p2 := cedar.NewPolicyFromAST(ast.Permit())
-		ps.Upsert("a wavering policy", p2)
+		ps.Set("a wavering policy", p2)
 
 		testutil.Equals(t, ps.Get("a wavering policy"), p2)
 	})
 }
-
-// func TestUpsertPolicySet(t *testing.T) {
-// 	t.Parallel()
-// 	t.Run("empty dst", func(t *testing.T) {
-// 		t.Parallel()
-
-// 		policy0 := cedar.NewPolicyFromAST(ast.Forbid())
-
-// 		var policy1 cedar.Policy
-// 		testutil.OK(t, policy1.UnmarshalJSON(
-// 			[]byte(`{"effect":"permit","principal":{"op":"All"},"action":{"op":"All"},"resource":{"op":"All"}}`),
-// 		))
-
-// 		ps1 := cedar.NewPolicySet()
-// 		ps1.Upsert("policy0", policy0)
-// 		ps1.Upsert("policy1", policy1)
-
-// 		ps2 := cedar.NewPolicySet()
-// 		ps2.UpsertPolicySet(ps1)
-
-// 		testutil.Equals(t, ps2.Get("policy0"), policy0)
-// 		testutil.Equals(t, ps2.Get("policy1"), &policy1)
-// 		testutil.Equals(t, ps2.Get("policy2"), nil)
-// 	})
-// 	t.Run("upsert", func(t *testing.T) {
-// 		t.Parallel()
-
-// 		policyA := cedar.NewPolicyFromAST(ast.Forbid())
-
-// 		var policyB cedar.Policy
-// 		testutil.OK(t, policyB.UnmarshalJSON(
-// 			[]byte(`{"effect":"permit","principal":{"op":"All"},"action":{"op":"All"},"resource":{"op":"All"}}`),
-// 		))
-
-// 		policyC := cedar.NewPolicyFromAST(ast.Permit())
-
-// 		// ps1 maps 0 -> A and 1 -> B
-// 		ps1 := cedar.NewPolicySet()
-// 		ps1.Upsert("policy0", policyA)
-// 		ps1.Upsert("policy1", &policyB)
-
-// 		// ps1 maps 0 -> b and 2 -> C
-// 		ps2 := cedar.NewPolicySet()
-// 		ps2.Upsert("policy0", &policyB)
-// 		ps2.Upsert("policy2", policyC)
-
-// 		// Upsert should clobber ps2's policy0, insert policy1, and leave policy2 untouched
-// 		ps2.UpsertPolicySet(ps1)
-
-// 		testutil.Equals(t, ps2.Get("policy0"), policyA)
-// 		testutil.Equals(t, ps2.Get("policy1"), &policyB)
-// 		testutil.Equals(t, ps2.Get("policy2"), policyC)
-// 	})
-// }
 
 func TestDeletePolicy(t *testing.T) {
 	t.Parallel()
@@ -134,7 +80,7 @@ func TestDeletePolicy(t *testing.T) {
 		ps := cedar.NewPolicySet()
 
 		p1 := cedar.NewPolicyFromAST(ast.Forbid())
-		ps.Upsert("a policy", p1)
+		ps.Set("a policy", p1)
 		ps.Delete("a policy")
 
 		testutil.Equals(t, ps.Get("a policy"), cedar.Policy{})
@@ -163,11 +109,20 @@ forbid (
 	ps := cedar.NewPolicySet()
 	for i, p := range policies {
 		p.SetFilename("example.cedar")
-		ps.Upsert(cedar.PolicyID(fmt.Sprintf("policy%d", i)), p)
+		ps.Set(cedar.PolicyID(fmt.Sprintf("policy%d", i)), p)
 	}
 
 	testutil.Equals(t, ps.Get("policy0").Effect(), cedar.Permit)
 	testutil.Equals(t, ps.Get("policy1").Effect(), cedar.Forbid)
 
 	testutil.Equals(t, string(ps.MarshalCedar()), policiesStr)
+
+}
+
+func TestPolicyMap(t *testing.T) {
+	t.Parallel()
+	ps, err := cedar.NewPolicySetFromBytes("", []byte(`permit (principal, action, resource);`))
+	testutil.OK(t, err)
+	m := ps.Map()
+	testutil.Equals(t, len(m), 1)
 }
