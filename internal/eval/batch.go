659fe02ef53bd49e0d2b844ae541ed39c1ed3745
philhassey <phil@strongdm.com>
2024-08-23 21:59:58 -0600

    internal/eval: lazy and ordered compilation
    Addresses IDX-46
    
    Signed-off-by: philhassey <phil@strongdm.com>

--- internal/eval/batch.go
+++ internal/eval/batch.go
@@ -18,35 +18,34 @@ type BatchResult struct {
 	Decision                    bool
 }
 
-type batchPolicy struct {
-	ast    *ast.Policy
-	evaler Evaler
+type batcher struct {
+	policies []*ast.Policy
+	compiled bool
+	forbids  []Evaler
+	permits  []Evaler
 }
 
 func PubBatch(policies []*publicast.Policy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
-	ps := make([]batchPolicy, len(policies))
+	var res batcher
+	res.policies = make([]*ast.Policy, len(policies))
 	for i, pub := range policies {
 		p := (*ast.Policy)(pub)
-		ps[i] = batchPolicy{
-			ast:    p,
-			evaler: Compile(p),
-		}
+		res.policies[i] = p
 	}
-	batch(ps, entityMap, request, cb)
+	batch(&res, entityMap, request, cb)
 }
 
 func Batch(policies []*ast.Policy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
-	ps := make([]batchPolicy, len(policies))
-	for i, p := range policies {
-		ps[i] = batchPolicy{
-			ast:    p,
-			evaler: Compile(p),
-		}
+	var res batcher
+	res.policies = make([]*ast.Policy, len(policies))
+	for i, pub := range policies {
+		p := (*ast.Policy)(pub)
+		res.policies[i] = p
 	}
-	batch(ps, entityMap, request, cb)
+	batch(&res, entityMap, request, cb)
 }
 
-func batch(policies []batchPolicy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
+func batch(b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
 	pl, al, rl := len(request.Principals), len(request.Actions), len(request.Resources)
 	if pl == 0 || al == 0 || rl == 0 {
 		return
@@ -58,7 +57,7 @@ func batch(policies []batchPolicy, entityMap types.Entities, request BatchReques
 			Action:    request.Actions[0],
 			Resource:  request.Resources[0],
 		}
-		ok := batchAuthz(policies, ctx)
+		ok := batchAuthz(b, ctx)
 		cb(BatchResult{
 			Principal: request.Principals[0],
 			Action:    request.Actions[0],
@@ -80,49 +79,46 @@ func batch(policies []batchPolicy, entityMap types.Entities, request BatchReques
 		if rl == 1 {
 			ctx.Resource = request.Resources[0]
 		}
-		var np []batchPolicy
-		for _, p := range policies {
-			p, keep := partialPolicy(ctx, p.ast)
+		var nb batcher
+		for _, p := range b.policies {
+			p, keep := partialPolicy(ctx, p)
 			if !keep {
 				continue
 			}
-			np = append(np, batchPolicy{
-				ast:    p,
-				evaler: Compile(p),
-			})
+			nb.policies = append(nb.policies, p)
 		}
 		// fmt.Println("partial", pl, al, rl)
-		policies = np
+		b = &nb
 	}
 
 	if pl > 1 && (al == 1 || pl <= al) && (rl == 1 || pl <= rl) {
-		batchPrincipal(policies, entityMap, request, cb)
+		batchPrincipal(b, entityMap, request, cb)
 	} else if al > 1 && (pl == 1 || al <= pl) && (rl == 1 || al <= rl) {
-		batchAction(policies, entityMap, request, cb)
+		batchAction(b, entityMap, request, cb)
 	} else {
-		batchResource(policies, entityMap, request, cb)
+		batchResource(b, entityMap, request, cb)
 	}
 }
 
-func batchPrincipal(policies []batchPolicy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
+func batchPrincipal(b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
 	in := request.Principals
 	for i := range in {
 		request.Principals = in[i : i+1]
-		batch(policies, entityMap, request, cb)
+		batch(b, entityMap, request, cb)
 	}
 }
-func batchAction(policies []batchPolicy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
+func batchAction(b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
 	in := request.Actions
 	for i := range in {
 		request.Actions = in[i : i+1]
-		batch(policies, entityMap, request, cb)
+		batch(b, entityMap, request, cb)
 	}
 }
-func batchResource(policies []batchPolicy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
+func batchResource(b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) {
 	in := request.Resources
 	for i := range in {
 		request.Resources = in[i : i+1]
-		batch(policies, entityMap, request, cb)
+		batch(b, entityMap, request, cb)
 	}
 }
 
@@ -133,12 +129,11 @@ func batchResource(policies []batchPolicy, entityMap types.Entities, request Bat
 // 	fmt.Println(got.String())
 // }
 
-func batchAuthz(policies []batchPolicy, ctx *Context) bool {
-	for _, p := range policies {
-		if p.ast.Effect != ast.EffectForbid {
-			continue
-		}
-		v, err := p.evaler.Eval(ctx)
+func batchAuthz(b *batcher, ctx *Context) bool {
+	batchCompile(b)
+
+	for _, p := range b.forbids {
+		v, err := p.Eval(ctx)
 		if err != nil {
 			continue
 		}
@@ -146,11 +141,8 @@ func batchAuthz(policies []batchPolicy, ctx *Context) bool {
 			return false
 		}
 	}
-	for _, p := range policies {
-		if p.ast.Effect != ast.EffectPermit {
-			continue
-		}
-		v, err := p.evaler.Eval(ctx)
+	for _, p := range b.permits {
+		v, err := p.Eval(ctx)
 		if err != nil {
 			continue
 		}
@@ -159,4 +151,18 @@ func batchAuthz(policies []batchPolicy, ctx *Context) bool {
 		}
 	}
 	return false
+}
+
+func batchCompile(b *batcher) {
+	if b.compiled {
+		return
+	}
+	for _, p := range b.policies {
+		if p.Effect == ast.EffectPermit {
+			b.permits = append(b.permits, Compile(p))
+		} else {
+			b.forbids = append(b.forbids, Compile(p))
+		}
+	}
+	b.compiled = true
 }
