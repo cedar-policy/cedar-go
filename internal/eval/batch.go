ee9fa1e316be8636aa8058bbbd6b562701c132ec
philhassey <phil@strongdm.com>
2024-09-05 12:22:28 -0600

    internal/eval: remove original batch implementation
    Addresses IDX-50
    
    Signed-off-by: philhassey <phil@strongdm.com>

--- internal/eval/batch.go
+++ internal/eval/batch.go
@@ -11,143 +11,177 @@ import (
 )
 
 type BatchRequest struct {
-	Principals []types.EntityUID
-	Actions    []types.EntityUID
-	Resources  []types.EntityUID
-	Context    types.Record
+	Principal types.Value
+	Action    types.Value
+	Resource  types.Value
+	Context   types.Value
+	Variables Variables
+}
+
+type batchRequestState struct {
+	Principal types.Value
+	Action    types.Value
+	Resource  types.Value
+	Context   types.Value
+	Variables []variableItem
+	Values    Values
+}
+
+type variableItem struct {
+	Key    types.String
+	Values []types.Value
+}
+
+type Variables map[types.String][]types.Value
+
+const variableEntityType = "__cedar::variable"
+
+func Variable(v types.String) types.Value {
+	return types.NewEntityUID(variableEntityType, v)
 }
 
 type BatchResult struct {
-	Principal, Action, Resource types.EntityUID
-	Decision                    bool
+	Principal types.EntityUID
+	Action    types.EntityUID
+	Resource  types.EntityUID
+	Context   types.Record
+	Decision  bool
+	Values    Values
 }
+type Values map[types.String]types.Value
 
-type batcher struct {
-	policies []*ast.Policy
-	compiled bool
-	forbids  []Evaler
-	permits  []Evaler
-	evalCtx  *Context
-}
-
-// PubBatch will run a batch of authorization evaluations.  It will only error in case of early termination.
 func PubBatch(ctx context.Context, policies []*publicast.Policy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) error {
-	var res batcher
-	res.policies = make([]*ast.Policy, len(policies))
+	pol2 := make([]*ast.Policy, len(policies))
 	for i, pub := range policies {
 		p := (*ast.Policy)(pub)
-		res.policies[i] = p
+		pol2[i] = p
 	}
-	res.evalCtx = PrepContext(&Context{})
-	return batch(ctx, &res, entityMap, request, cb)
+	return Batch(ctx, pol2, entityMap, request, cb)
 }
 
-// Batch will run a batch of authorization evaluations.  It will only error in case of early termination.
+// Batch will run a batch of authorization evaluations.
+// It will error in case of early termination.
+// It will error in case any of PARC are an incorrect type at eval type.
+// The result passed to the callback must be used immediately and not modified.
 func Batch(ctx context.Context, policies []*ast.Policy, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) error {
-	var res batcher
-	res.policies = make([]*ast.Policy, len(policies))
-	for i, pub := range policies {
-		p := (*ast.Policy)(pub)
-		res.policies[i] = p
+	var be batchEvaler
+	be.policies = policies
+	be.evalCtx = PrepContext(&Context{})
+	state := batchRequestState{
+		Principal: request.Principal,
+		Action:    request.Action,
+		Resource:  request.Resource,
+		Context:   request.Context,
+		Values:    Values{},
 	}
-	res.evalCtx = PrepContext(&Context{})
-	return batch(ctx, &res, entityMap, request, cb)
+	for k, v := range request.Variables {
+		state.Variables = append(state.Variables, variableItem{Key: k, Values: v})
+	}
+	slices.SortFunc(state.Variables, func(a, b variableItem) int {
+		return len(a.Values) - len(b.Values)
+	})
+	return doBatch(ctx, &be, entityMap, state, cb)
 }
 
-func batch(ctx context.Context, b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) error {
-	pl, al, rl := len(request.Principals), len(request.Actions), len(request.Resources)
-	if pl == 0 || al == 0 || rl == 0 {
-		return nil
-	}
-
+func doBatch(ctx context.Context, be *batchEvaler, entityMap types.Entities, request batchRequestState, cb func(BatchResult)) error {
 	// check for context cancellation only if there is more work to be done
 	if err := ctx.Err(); err != nil {
 		return err
 	}
 
-	if pl == 1 && al == 1 && rl == 1 {
+	if len(request.Variables) == 0 {
+		var res BatchResult
+		var err error
+		if res.Principal, err = ValueToEntity(request.Principal); err != nil {
+			return err
+		}
+		if res.Action, err = ValueToEntity(request.Action); err != nil {
+			return err
+		}
+		if res.Resource, err = ValueToEntity(request.Resource); err != nil {
+			return err
+		}
+		if res.Context, err = ValueToRecord(request.Context); err != nil {
+			return err
+		}
 		ctx := &Context{
 			Entities:  entityMap,
-			Principal: request.Principals[0],
-			Action:    request.Actions[0],
-			Resource:  request.Resources[0],
+			Principal: request.Principal,
+			Action:    request.Action,
+			Resource:  request.Resource,
 			Context:   request.Context,
-			inCache:   b.evalCtx.inCache,
+			inCache:   be.evalCtx.inCache,
 		}
-		ok := batchAuthz(b, ctx)
-		cb(BatchResult{
-			Principal: request.Principals[0],
-			Action:    request.Actions[0],
-			Resource:  request.Resources[0],
-			Decision:  ok,
-		})
+		res.Decision = batchAuthz(be, ctx)
+		res.Values = request.Values
+		cb(res)
 		return nil
 	}
 
-	// apply partial evaluation
-	if pl == 1 || al == 1 || rl == 1 {
-		evalCtx := &Context{Entities: entityMap, inCache: b.evalCtx.inCache}
-		if pl == 1 {
-			evalCtx.Principal = request.Principals[0]
+	// else, partial eval what we have so far
+	var np []*ast.Policy
+	for _, p := range be.policies {
+		p, keep, _ := partialPolicy(&Context{
+			Entities:  entityMap,
+			inCache:   be.evalCtx.inCache,
+			Principal: request.Principal,
+			Action:    request.Action,
+			Resource:  request.Resource,
+			Context:   request.Context,
+		}, p)
+		if !keep {
+			continue
 		}
-		if al == 1 {
-			evalCtx.Action = request.Actions[0]
-		}
-		if rl == 1 {
-			evalCtx.Resource = request.Resources[0]
-		}
-		var np []*ast.Policy
-		for _, p := range b.policies {
-			p, keep, _ := partialPolicy(evalCtx, p)
-			if !keep {
-				continue
-			}
-			np = append(np, p)
-		}
-		b = &batcher{
-			evalCtx:  b.evalCtx,
-			policies: np,
-		}
+		np = append(np, p)
+	}
+	be = &batchEvaler{
+		evalCtx:  be.evalCtx,
+		policies: np,
 	}
 
-	if pl > 1 && (al == 1 || pl <= al) && (rl == 1 || pl <= rl) {
-		return batchPrincipal(ctx, b, entityMap, request, cb)
-	} else if al > 1 && (pl == 1 || al <= pl) && (rl == 1 || al <= rl) {
-		return batchAction(ctx, b, entityMap, request, cb)
-	} else {
-		return batchResource(ctx, b, entityMap, request, cb)
+	// then loop the current unknowns
+	u := request.Variables[0]
+	_, chPrincipal := cloneSub(request.Principal, u.Key, nil)
+	_, chAction := cloneSub(request.Action, u.Key, nil)
+	_, chResource := cloneSub(request.Resource, u.Key, nil)
+	_, chContext := cloneSub(request.Context, u.Key, nil)
+	uks := request.Variables[1:]
+	for _, v := range u.Values {
+		child := batchRequestState{
+			Principal: request.Principal,
+			Action:    request.Action,
+			Resource:  request.Resource,
+			Context:   request.Context,
+			Variables: uks,
+			Values:    request.Values,
+		}
+		request.Values[u.Key] = v
+		if chPrincipal {
+			child.Principal, _ = cloneSub(request.Principal, u.Key, v)
+		}
+		if chAction {
+			child.Action, _ = cloneSub(request.Action, u.Key, v)
+		}
+		if chResource {
+			child.Resource, _ = cloneSub(request.Resource, u.Key, v)
+		}
+		if chContext {
+			child.Context, _ = cloneSub(request.Context, u.Key, v)
+		}
+		if err := doBatch(ctx, be, entityMap, child, cb); err != nil {
+			return err
+		}
 	}
+	delete(request.Values, u.Key)
+	return nil
 }
 
-func batchPrincipal(ctx context.Context, b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) error {
-	in := request.Principals
-	for i := range in {
-		request.Principals = in[i : i+1]
-		if err := batch(ctx, b, entityMap, request, cb); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-func batchAction(ctx context.Context, b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) error {
-	in := request.Actions
-	for i := range in {
-		request.Actions = in[i : i+1]
-		if err := batch(ctx, b, entityMap, request, cb); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-func batchResource(ctx context.Context, b *batcher, entityMap types.Entities, request BatchRequest, cb func(BatchResult)) error {
-	in := request.Resources
-	for i := range in {
-		request.Resources = in[i : i+1]
-		if err := batch(ctx, b, entityMap, request, cb); err != nil {
-			return err
-		}
-	}
-	return nil
+type batchEvaler struct {
+	policies []*ast.Policy
+	compiled bool
+	forbids  []Evaler
+	permits  []Evaler
+	evalCtx  *Context
 }
 
 // func testPrintPolicy(p *ast.Policy) {
@@ -157,7 +191,7 @@ func batchResource(ctx context.Context, b *batcher, entityMap types.Entities, re
 // 	fmt.Println(got.String())
 // }
 
-func batchAuthz(b *batcher, evalCtx *Context) bool {
+func batchAuthz(b *batchEvaler, evalCtx *Context) bool {
 	batchCompile(b)
 
 	for _, p := range b.forbids {
@@ -181,7 +215,7 @@ func batchAuthz(b *batcher, evalCtx *Context) bool {
 	return false
 }
 
-func batchCompile(b *batcher) {
+func batchCompile(b *batchEvaler) {
 	if b.compiled {
 		return
 	}
@@ -193,98 +227,6 @@ func batchCompile(b *batcher) {
 		}
 	}
 	b.compiled = true
-}
-
-type AdvancedBatchRequest struct {
-	Principal types.Value
-	Action    types.Value
-	Resource  types.Value
-	Context   types.Value
-	Variables Variables
-}
-
-type sortedAdvancedBatchRequest struct {
-	Principal types.Value
-	Action    types.Value
-	Resource  types.Value
-	Context   types.Value
-	Variables []variableItem
-	Values    Values
-}
-
-type variableItem struct {
-	Key    types.String
-	Values []types.Value
-}
-
-type Variables map[types.String][]types.Value
-
-const variableEntityType = "__cedar::variable"
-
-func Variable(v types.String) types.Value {
-	return types.NewEntityUID(variableEntityType, v)
-}
-
-type AdvancedBatchResult struct {
-	Principal types.EntityUID
-	Action    types.EntityUID
-	Resource  types.EntityUID
-	Context   types.Record
-	Decision  bool
-	Values    Values
-}
-type Values map[types.String]types.Value
-
-func PubAdvancedBatch(ctx context.Context, policies []*publicast.Policy, entityMap types.Entities, request AdvancedBatchRequest, cb func(AdvancedBatchResult)) error {
-	var res batcher
-	res.policies = make([]*ast.Policy, len(policies))
-	for i, pub := range policies {
-		p := (*ast.Policy)(pub)
-		res.policies[i] = p
-	}
-	res.evalCtx = PrepContext(&Context{})
-	sReq := sortedAdvancedBatchRequest{
-		Principal: request.Principal,
-		Action:    request.Action,
-		Resource:  request.Resource,
-		Context:   request.Context,
-		Values:    Values{},
-	}
-	for k, v := range request.Variables {
-		sReq.Variables = append(sReq.Variables, variableItem{Key: k, Values: v})
-	}
-	slices.SortStableFunc(sReq.Variables, func(a, b variableItem) int {
-		return len(a.Values) - len(b.Values)
-	})
-	return advancedBatch(ctx, &res, entityMap, sReq, cb)
-}
-
-// AdvancedBatch will run a batch of authorization evaluations.
-// It will error in case of early termination.
-// It will error in case any of PARC are an incorrect type at eval type.
-// The result passed to the callback must be used immediately and not modified.
-func AdvancedBatch(ctx context.Context, policies []*ast.Policy, entityMap types.Entities, request AdvancedBatchRequest, cb func(AdvancedBatchResult)) error {
-	var res batcher
-	res.policies = make([]*ast.Policy, len(policies))
-	for i, pub := range policies {
-		p := (*ast.Policy)(pub)
-		res.policies[i] = p
-	}
-	res.evalCtx = PrepContext(&Context{})
-	sReq := sortedAdvancedBatchRequest{
-		Principal: request.Principal,
-		Action:    request.Action,
-		Resource:  request.Resource,
-		Context:   request.Context,
-		Values:    Values{},
-	}
-	for k, v := range request.Variables {
-		sReq.Variables = append(sReq.Variables, variableItem{Key: k, Values: v})
-	}
-	slices.SortStableFunc(sReq.Variables, func(a, b variableItem) int {
-		return len(a.Values) - len(b.Values)
-	})
-	return advancedBatch(ctx, &res, entityMap, sReq, cb)
 }
 
 // cloneSub will return a new value if any of its children have changed
@@ -321,95 +263,4 @@ func cloneSub(r types.Value, k types.String, v types.Value) (types.Value, bool) 
 		return t, cloned
 	}
 	return r, false
-}
-
-func advancedBatch(ctx context.Context, b *batcher, entityMap types.Entities, request sortedAdvancedBatchRequest, cb func(AdvancedBatchResult)) error {
-	// check for context cancellation only if there is more work to be done
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-
-	if len(request.Variables) == 0 {
-		var res AdvancedBatchResult
-		var err error
-		if res.Principal, err = ValueToEntity(request.Principal); err != nil {
-			return err
-		}
-		if res.Action, err = ValueToEntity(request.Action); err != nil {
-			return err
-		}
-		if res.Resource, err = ValueToEntity(request.Resource); err != nil {
-			return err
-		}
-		if res.Context, err = ValueToRecord(request.Context); err != nil {
-			return err
-		}
-		ctx := &Context{
-			Entities:  entityMap,
-			Principal: request.Principal,
-			Action:    request.Action,
-			Resource:  request.Resource,
-			Context:   request.Context,
-			inCache:   b.evalCtx.inCache,
-		}
-		res.Decision = batchAuthz(b, ctx)
-		res.Values = request.Values
-		cb(res)
-		return nil
-	}
-
-	// else, partial eval what we have so far
-	evalCtx := &Context{Entities: entityMap, inCache: b.evalCtx.inCache}
-	evalCtx.Principal = request.Principal
-	evalCtx.Action = request.Action
-	evalCtx.Resource = request.Resource
-	evalCtx.Context = request.Context
-	var np []*ast.Policy
-	for _, p := range b.policies {
-		p, keep, _ := partialPolicy(evalCtx, p)
-		if !keep {
-			continue
-		}
-		np = append(np, p)
-	}
-	b = &batcher{
-		evalCtx:  b.evalCtx,
-		policies: np,
-	}
-
-	// then loop the current unknowns
-	u := request.Variables[0]
-	_, chPrincipal := cloneSub(request.Principal, u.Key, types.True)
-	_, chAction := cloneSub(request.Action, u.Key, types.True)
-	_, chResource := cloneSub(request.Resource, u.Key, types.True)
-	_, chContext := cloneSub(request.Context, u.Key, types.True)
-	uks := request.Variables[1:]
-	for _, v := range u.Values {
-		child := sortedAdvancedBatchRequest{
-			Principal: request.Principal,
-			Action:    request.Action,
-			Resource:  request.Resource,
-			Context:   request.Context,
-			Variables: uks,
-			Values:    request.Values,
-		}
-		request.Values[u.Key] = v
-		if chPrincipal {
-			child.Principal, _ = cloneSub(request.Principal, u.Key, v)
-		}
-		if chAction {
-			child.Action, _ = cloneSub(request.Action, u.Key, v)
-		}
-		if chResource {
-			child.Resource, _ = cloneSub(request.Resource, u.Key, v)
-		}
-		if chContext {
-			child.Context, _ = cloneSub(request.Context, u.Key, v)
-		}
-		if err := advancedBatch(ctx, b, entityMap, child, cb); err != nil {
-			return err
-		}
-	}
-	delete(request.Values, u.Key)
-	return nil
 }
--- internal/eval/batch_test.go
+++ internal/eval/batch_test.go
@@ -28,119 +28,6 @@ func TestBatch(t *testing.T) {
 			ast.Permit(),
 			types.Entities{},
 			BatchRequest{
-				Principals: []types.EntityUID{p1},
-				Actions:    []types.EntityUID{a1, a2},
-				Resources:  []types.EntityUID{r1, r2, r3},
-			},
-			[]BatchResult{
-				{Principal: p1, Action: a1, Resource: r1, Decision: true},
-				{Principal: p1, Action: a1, Resource: r2, Decision: true},
-				{Principal: p1, Action: a1, Resource: r3, Decision: true},
-				{Principal: p1, Action: a2, Resource: r1, Decision: true},
-				{Principal: p1, Action: a2, Resource: r2, Decision: true},
-				{Principal: p1, Action: a2, Resource: r3, Decision: true},
-			},
-		},
-
-		{"someOk",
-			ast.Permit().PrincipalEq(p1).ActionEq(a2).ResourceEq(r3),
-			types.Entities{},
-			BatchRequest{
-				Principals: []types.EntityUID{p1},
-				Actions:    []types.EntityUID{a1, a2},
-				Resources:  []types.EntityUID{r1, r2, r3},
-			},
-			[]BatchResult{
-				{Principal: p1, Action: a1, Resource: r1, Decision: false},
-				{Principal: p1, Action: a1, Resource: r2, Decision: false},
-				{Principal: p1, Action: a1, Resource: r3, Decision: false},
-				{Principal: p1, Action: a2, Resource: r1, Decision: false},
-				{Principal: p1, Action: a2, Resource: r2, Decision: false},
-				{Principal: p1, Action: a2, Resource: r3, Decision: true},
-			},
-		},
-
-		{"attributeAccess",
-			ast.Permit().When(ast.Principal().Access("tags").Has("a").And(ast.Principal().Access("tags").Access("a").Equal(ast.String("a")))),
-			types.Entities{
-				p1: {
-					UID: p1,
-					Attributes: types.Record{
-						"tags": types.Record{"a": types.String("a")},
-					},
-				},
-				p2: {
-					UID: p2,
-					Attributes: types.Record{
-						"tags": types.Record{"b": types.String("b")},
-					},
-				},
-			},
-			BatchRequest{
-				Principals: []types.EntityUID{p1, p2},
-				Actions:    []types.EntityUID{a1},
-				Resources:  []types.EntityUID{r1, r2},
-			},
-			[]BatchResult{
-				{Principal: p1, Action: a1, Resource: r1, Decision: true},
-				{Principal: p1, Action: a1, Resource: r2, Decision: true},
-				{Principal: p2, Action: a1, Resource: r1, Decision: false},
-				{Principal: p2, Action: a1, Resource: r2, Decision: false},
-			},
-		},
-
-		{"contextAccess",
-			ast.Permit().When(ast.Context().Access("key").Equal(ast.Long(42))),
-			types.Entities{},
-			BatchRequest{
-				Principals: []types.EntityUID{p1},
-				Actions:    []types.EntityUID{a1, a2},
-				Resources:  []types.EntityUID{r1, r2, r3},
-				Context: types.Record{
-					"key": types.Long(42),
-				},
-			},
-			[]BatchResult{
-				{Principal: p1, Action: a1, Resource: r1, Decision: true},
-				{Principal: p1, Action: a1, Resource: r2, Decision: true},
-				{Principal: p1, Action: a1, Resource: r3, Decision: true},
-				{Principal: p1, Action: a2, Resource: r1, Decision: true},
-				{Principal: p1, Action: a2, Resource: r2, Decision: true},
-				{Principal: p1, Action: a2, Resource: r3, Decision: true},
-			},
-		},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			var res []BatchResult
-			err := Batch(context.Background(), []*ast.Policy{tt.policy}, tt.entities, tt.request, func(br BatchResult) {
-				res = append(res, br)
-			})
-			testutil.OK(t, err)
-			testutil.Equals(t, res, tt.results)
-		})
-	}
-}
-
-func TestAdvancedBatch(t *testing.T) {
-	t.Parallel()
-	p1, p2, p3 := types.NewEntityUID("P", "1"), types.NewEntityUID("P", "2"), types.NewEntityUID("P", "3")
-	a1, a2, a3 := types.NewEntityUID("A", "1"), types.NewEntityUID("A", "2"), types.NewEntityUID("A", "3")
-	r1, r2, r3 := types.NewEntityUID("R", "1"), types.NewEntityUID("R", "2"), types.NewEntityUID("R", "3")
-	_, _, _, _, _, _, _, _, _ = p1, p2, p3, a1, a2, a3, r1, r2, r3
-	tests := []struct {
-		name     string
-		policy   *ast.Policy
-		entities types.Entities
-		request  AdvancedBatchRequest
-		results  []AdvancedBatchResult
-	}{
-		{"smokeTest",
-			ast.Permit(),
-			types.Entities{},
-			AdvancedBatchRequest{
 				Principal: p1,
 				Action:    Variable("action"),
 				Resource:  Variable("resource"),
@@ -150,7 +37,7 @@ func TestAdvancedBatch(t *testing.T) {
 					"resource": []types.Value{r1, r2, r3},
 				},
 			},
-			[]AdvancedBatchResult{
+			[]BatchResult{
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{}, Decision: true, Values: Values{"action": a1, "resource": r1}},
 				{Principal: p1, Action: a1, Resource: r2, Context: types.Record{}, Decision: true, Values: Values{"action": a1, "resource": r2}},
 				{Principal: p1, Action: a1, Resource: r3, Context: types.Record{}, Decision: true, Values: Values{"action": a1, "resource": r3}},
@@ -163,7 +50,7 @@ func TestAdvancedBatch(t *testing.T) {
 		{"someOk",
 			ast.Permit().PrincipalEq(p1).ActionEq(a2).ResourceEq(r3),
 			types.Entities{},
-			AdvancedBatchRequest{
+			BatchRequest{
 				Principal: p1,
 				Action:    Variable("action"),
 				Resource:  Variable("resource"),
@@ -173,7 +60,7 @@ func TestAdvancedBatch(t *testing.T) {
 					"resource": []types.Value{r1, r2, r3},
 				},
 			},
-			[]AdvancedBatchResult{
+			[]BatchResult{
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{}, Decision: false, Values: Values{"action": a1, "resource": r1}},
 				{Principal: p1, Action: a1, Resource: r2, Context: types.Record{}, Decision: false, Values: Values{"action": a1, "resource": r2}},
 				{Principal: p1, Action: a1, Resource: r3, Context: types.Record{}, Decision: false, Values: Values{"action": a1, "resource": r3}},
@@ -199,7 +86,7 @@ func TestAdvancedBatch(t *testing.T) {
 					},
 				},
 			},
-			AdvancedBatchRequest{
+			BatchRequest{
 				Principal: Variable("principal"),
 				Action:    a1,
 				Resource:  Variable("resource"),
@@ -209,7 +96,7 @@ func TestAdvancedBatch(t *testing.T) {
 					"resource":  []types.Value{r1, r2},
 				},
 			},
-			[]AdvancedBatchResult{
+			[]BatchResult{
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{}, Decision: true, Values: Values{"principal": p1, "resource": r1}},
 				{Principal: p1, Action: a1, Resource: r2, Context: types.Record{}, Decision: true, Values: Values{"principal": p1, "resource": r2}},
 				{Principal: p2, Action: a1, Resource: r1, Context: types.Record{}, Decision: false, Values: Values{"principal": p2, "resource": r1}},
@@ -220,7 +107,7 @@ func TestAdvancedBatch(t *testing.T) {
 		{"contextAccess",
 			ast.Permit().When(ast.Context().Access("key").Equal(ast.Long(42))),
 			types.Entities{},
-			AdvancedBatchRequest{
+			BatchRequest{
 				Principal: p1,
 				Action:    Variable("action"),
 				Resource:  Variable("resource"),
@@ -232,7 +119,7 @@ func TestAdvancedBatch(t *testing.T) {
 					"resource": []types.Value{r1, r2, r3},
 				},
 			},
-			[]AdvancedBatchResult{
+			[]BatchResult{
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{"key": types.Long(42)}, Decision: true, Values: Values{"action": a1, "resource": r1}},
 				{Principal: p1, Action: a1, Resource: r2, Context: types.Record{"key": types.Long(42)}, Decision: true, Values: Values{"action": a1, "resource": r2}},
 				{Principal: p1, Action: a1, Resource: r3, Context: types.Record{"key": types.Long(42)}, Decision: true, Values: Values{"action": a1, "resource": r3}},
@@ -245,7 +132,7 @@ func TestAdvancedBatch(t *testing.T) {
 		{"variableContext",
 			ast.Permit().When(ast.Context().Access("key").Equal(ast.Long(42))),
 			types.Entities{},
-			AdvancedBatchRequest{
+			BatchRequest{
 				Principal: p1,
 				Action:    a1,
 				Resource:  r1,
@@ -254,7 +141,7 @@ func TestAdvancedBatch(t *testing.T) {
 					"context": []types.Value{types.Record{"key": types.Long(41)}, types.Record{"key": types.Long(42)}, types.Record{"key": types.Long(43)}},
 				},
 			},
-			[]AdvancedBatchResult{
+			[]BatchResult{
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{"key": types.Long(41)}, Decision: false, Values: Values{"context": types.Record{"key": types.Long(41)}}},
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{"key": types.Long(42)}, Decision: true, Values: Values{"context": types.Record{"key": types.Long(42)}}},
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{"key": types.Long(43)}, Decision: false, Values: Values{"context": types.Record{"key": types.Long(43)}}},
@@ -264,7 +151,7 @@ func TestAdvancedBatch(t *testing.T) {
 		{"variableContextAccess",
 			ast.Permit().When(ast.Context().Access("key").Equal(ast.Long(42))),
 			types.Entities{},
-			AdvancedBatchRequest{
+			BatchRequest{
 				Principal: p1,
 				Action:    a1,
 				Resource:  r1,
@@ -275,7 +162,7 @@ func TestAdvancedBatch(t *testing.T) {
 					"key": []types.Value{types.Long(41), types.Long(42), types.Long(43)},
 				},
 			},
-			[]AdvancedBatchResult{
+			[]BatchResult{
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{"key": types.Long(41)}, Decision: false, Values: Values{"key": types.Long(41)}},
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{"key": types.Long(42)}, Decision: true, Values: Values{"key": types.Long(42)}},
 				{Principal: p1, Action: a1, Resource: r1, Context: types.Record{"key": types.Long(43)}, Decision: false, Values: Values{"key": types.Long(43)}},
@@ -286,8 +173,8 @@ func TestAdvancedBatch(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			var res []AdvancedBatchResult
-			err := AdvancedBatch(context.Background(), []*ast.Policy{tt.policy}, tt.entities, tt.request, func(br AdvancedBatchResult) {
+			var res []BatchResult
+			err := Batch(context.Background(), []*ast.Policy{tt.policy}, tt.entities, tt.request, func(br BatchResult) {
 				br.Context = maps.Clone(br.Context)
 				br.Values = maps.Clone(br.Values)
 				res = append(res, br)
@@ -301,7 +188,6 @@ func TestAdvancedBatch(t *testing.T) {
 				}
 				testutil.Equals(t, found, true)
 			}
-			testutil.Equals(t, res, tt.results)
 		})
 	}
 }
