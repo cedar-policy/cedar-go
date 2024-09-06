546e1c289b38ecac842885db2c0a7a748d9bd481
philhassey <phil@strongdm.com>
2024-09-06 13:27:08 -0600

    x/exp/batch: simplify flow of batch code
    Addresses IDX-50
    
    Signed-off-by: philhassey <phil@strongdm.com>

--- x/exp/batch/batch.go
+++ x/exp/batch/batch.go
@@ -20,13 +20,28 @@ type Request struct {
 	Variables Variables
 }
 
-type batchRequestState struct {
-	Principal types.Value
-	Action    types.Value
-	Resource  types.Value
-	Context   types.Value
+type idEvaler struct {
+	PolicyID types.PolicyID
+	Evaler   eval.BoolEvaler
+	Effect   types.Effect
+	Position types.Position
+}
+
+type idPolicy struct {
+	PolicyID types.PolicyID
+	Policy   *ast.Policy
+}
+
+type batchEvaler struct {
 	Variables []variableItem
 	Values    Values
+
+	policies []idPolicy
+	compiled bool
+	// policySet *cedar.PolicySet
+	evalers  []*idEvaler
+	env      *eval.Env
+	callback Callback
 }
 
 type variableItem struct {
@@ -77,160 +92,122 @@ func Authorize(ctx context.Context, ps *cedar.PolicySet, entityMap types.Entitie
 	case request.Context == nil:
 		return fmt.Errorf("batch missing context")
 	}
-	be.env = eval.NewEnv()
-	state := batchRequestState{
-		Principal: request.Principal,
-		Action:    request.Action,
-		Resource:  request.Resource,
-		Context:   request.Context,
-		Values:    Values{},
-	}
-	for k, v := range request.Variables {
-		state.Variables = append(state.Variables, variableItem{Key: k, Values: v})
-	}
-	slices.SortFunc(state.Variables, func(a, b variableItem) int {
-		return len(a.Values) - len(b.Values)
-	})
-	return doBatch(ctx, &be, entityMap, state)
-}
-
-func doBatch(ctx context.Context, be *batchEvaler, entityMap types.Entities, request batchRequestState) error {
-	// check for context cancellation only if there is more work to be done
-	if err := ctx.Err(); err != nil {
-		return err
-	}
-
-	if len(request.Variables) == 0 {
-		return diagnosticAuthzWithCallback(be, entityMap, request)
-	}
-
-	// else, partial eval what we have so far
-	var np []idPolicy
-	for _, p := range be.policies {
-		part, keep, _ := eval.PartialPolicy(eval.InitEnvWithCacheFrom(&eval.Env{
-			Entities:  entityMap,
-			Principal: request.Principal,
-			Action:    request.Action,
-			Resource:  request.Resource,
-			Context:   request.Context,
-		}, be.env), p.Policy)
-		if !keep {
-			continue
-		}
-		np = append(np, idPolicy{PolicyID: p.PolicyID, Policy: part})
-	}
-	be = &batchEvaler{
-		env:      be.env,
-		policies: np,
-		callback: be.callback,
-	}
-
-	// if no more partial evaluation, fill in ignores with defaults
-	if len(request.Variables) == 1 {
-		if eval.IsIgnore(request.Principal) {
-			request.Principal = unknownEntity()
-		}
-		if eval.IsIgnore(request.Action) {
-			request.Action = unknownEntity()
-		}
-		if eval.IsIgnore(request.Resource) {
-			request.Resource = unknownEntity()
-		}
-		if eval.IsIgnore(request.Context) {
-			request.Context = types.Record{}
-		}
-	}
-
-	// then loop the current unknowns
-	u := request.Variables[0]
-	_, chPrincipal := cloneSub(request.Principal, u.Key, nil)
-	_, chAction := cloneSub(request.Action, u.Key, nil)
-	_, chResource := cloneSub(request.Resource, u.Key, nil)
-	_, chContext := cloneSub(request.Context, u.Key, nil)
-	uks := request.Variables[1:]
-	for _, v := range u.Values {
-		child := batchRequestState{
-			Principal: request.Principal,
-			Action:    request.Action,
-			Resource:  request.Resource,
-			Context:   request.Context,
-			Variables: uks,
-			Values:    request.Values,
-		}
-		request.Values[u.Key] = v
-		if chPrincipal {
-			child.Principal, _ = cloneSub(request.Principal, u.Key, v)
-		}
-		if chAction {
-			child.Action, _ = cloneSub(request.Action, u.Key, v)
-		}
-		if chResource {
-			child.Resource, _ = cloneSub(request.Resource, u.Key, v)
-		}
-		if chContext {
-			child.Context, _ = cloneSub(request.Context, u.Key, v)
-		}
-		if err := doBatch(ctx, be, entityMap, child); err != nil {
-			return err
-		}
-	}
-	delete(request.Values, u.Key)
-	return nil
-}
-
-type idEvaler struct {
-	PolicyID types.PolicyID
-	Evaler   eval.BoolEvaler
-	Effect   types.Effect
-	Position types.Position
-}
-
-type idPolicy struct {
-	PolicyID types.PolicyID
-	Policy   *ast.Policy
-}
-
-type batchEvaler struct {
-	policies []idPolicy
-	compiled bool
-	// policySet *cedar.PolicySet
-	evalers  []*idEvaler
-	env      *eval.Env
-	callback Callback
-}
-
-func diagnosticAuthzWithCallback(be *batchEvaler, entityMap types.Entities, request batchRequestState) error {
-	var res Result
-	var err error
-	if res.Request.Principal, err = eval.ValueToEntity(request.Principal); err != nil {
-		return err
-	}
-	if res.Request.Action, err = eval.ValueToEntity(request.Action); err != nil {
-		return err
-	}
-	if res.Request.Resource, err = eval.ValueToEntity(request.Resource); err != nil {
-		return err
-	}
-	if res.Request.Context, err = eval.ValueToRecord(request.Context); err != nil {
-		return err
-	}
-	res.Values = request.Values
-	batchCompile(be)
-	// TODO: is there a way to share a cache across requests when using cedar.PolicySet?
-	// res.Decision, res.Diagnostic = be.policySet.IsAuthorized(entityMap, res.Request)
-	res.Decision, res.Diagnostic = isAuthorized(be.env, be.evalers, entityMap, request)
-	be.callback(res)
-	return nil
-}
-
-func isAuthorized(parent *eval.Env, ps []*idEvaler, entityMap types.Entities, request batchRequestState) (types.Decision, types.Diagnostic) {
-	c := eval.InitEnvWithCacheFrom(&eval.Env{
+	be.env = eval.InitEnv(&eval.Env{
 		Entities:  entityMap,
 		Principal: request.Principal,
 		Action:    request.Action,
 		Resource:  request.Resource,
 		Context:   request.Context,
-	}, parent)
+	})
+	be.Values = Values{}
+	for k, v := range request.Variables {
+		be.Variables = append(be.Variables, variableItem{Key: k, Values: v})
+	}
+	slices.SortFunc(be.Variables, func(a, b variableItem) int {
+		return len(a.Values) - len(b.Values)
+	})
+	return doBatch(ctx, &be)
+}
+
+func doBatch(ctx context.Context, be *batchEvaler) error {
+	// check for context cancellation only if there is more work to be done
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	if len(be.Variables) == 0 {
+		return diagnosticAuthzWithCallback(be)
+	}
+
+	prevState := *be
+
+	// else, partial eval what we have so far
+	var np []idPolicy
+	for _, p := range be.policies {
+		part, keep, _ := eval.PartialPolicy(be.env, p.Policy)
+		if !keep {
+			continue
+		}
+		np = append(np, idPolicy{PolicyID: p.PolicyID, Policy: part})
+	}
+	be.compiled = false
+	be.policies = np
+	be.evalers = nil
+
+	// if no more partial evaluation, fill in ignores with defaults
+	if len(be.Variables) == 1 {
+		if eval.IsIgnore(be.env.Principal) {
+			be.env.Principal = unknownEntity()
+		}
+		if eval.IsIgnore(be.env.Action) {
+			be.env.Action = unknownEntity()
+		}
+		if eval.IsIgnore(be.env.Resource) {
+			be.env.Resource = unknownEntity()
+		}
+		if eval.IsIgnore(be.env.Context) {
+			be.env.Context = types.Record{}
+		}
+	}
+	loopEnv := *be.env
+
+	// then loop the current unknowns
+	u := be.Variables[0]
+	_, chPrincipal := cloneSub(be.env.Principal, u.Key, nil)
+	_, chAction := cloneSub(be.env.Action, u.Key, nil)
+	_, chResource := cloneSub(be.env.Resource, u.Key, nil)
+	_, chContext := cloneSub(be.env.Context, u.Key, nil)
+	be.Variables = be.Variables[1:]
+	be.Values = maps.Clone(be.Values)
+
+	for _, v := range u.Values {
+		*be.env = loopEnv
+		be.Values[u.Key] = v
+		if chPrincipal {
+			be.env.Principal, _ = cloneSub(loopEnv.Principal, u.Key, v)
+		}
+		if chAction {
+			be.env.Action, _ = cloneSub(loopEnv.Action, u.Key, v)
+		}
+		if chResource {
+			be.env.Resource, _ = cloneSub(loopEnv.Resource, u.Key, v)
+		}
+		if chContext {
+			be.env.Context, _ = cloneSub(loopEnv.Context, u.Key, v)
+		}
+		if err := doBatch(ctx, be); err != nil {
+			return err
+		}
+	}
+	*be = prevState
+	return nil
+}
+
+func diagnosticAuthzWithCallback(be *batchEvaler) error {
+	var res Result
+	var err error
+	if res.Request.Principal, err = eval.ValueToEntity(be.env.Principal); err != nil {
+		return err
+	}
+	if res.Request.Action, err = eval.ValueToEntity(be.env.Action); err != nil {
+		return err
+	}
+	if res.Request.Resource, err = eval.ValueToEntity(be.env.Resource); err != nil {
+		return err
+	}
+	if res.Request.Context, err = eval.ValueToRecord(be.env.Context); err != nil {
+		return err
+	}
+	res.Values = be.Values
+	batchCompile(be)
+	// TODO: is there a way to share a cache across requests when using cedar.PolicySet?
+	// res.Decision, res.Diagnostic = be.policySet.IsAuthorized(entityMap, res.Request)
+	res.Decision, res.Diagnostic = isAuthorized(be.evalers, be.env)
+	be.callback(res)
+	return nil
+}
+
+func isAuthorized(ps []*idEvaler, env *eval.Env) (types.Decision, types.Diagnostic) {
 	var diag types.Diagnostic
 	var forbids []types.DiagnosticReason
 	var permits []types.DiagnosticReason
@@ -240,7 +217,7 @@ func isAuthorized(parent *eval.Env, ps []*idEvaler, entityMap types.Entities, re
 	// - For permit, all permits must be run to collect annotations
 	// - For forbid, forbids must be run to collect annotations
 	for _, po := range ps {
-		result, err := po.Evaler.Eval(c)
+		result, err := po.Evaler.Eval(env)
 		if err != nil {
 			diag.Errors = append(diag.Errors, types.DiagnosticError{PolicyID: po.PolicyID, Position: po.Position, Message: err.Error()})
 			continue
@@ -272,19 +249,19 @@ func isAuthorized(parent *eval.Env, ps []*idEvaler, entityMap types.Entities, re
 // 	fmt.Println(got.String())
 // }
 
-func batchCompile(b *batchEvaler) {
-	if b.compiled {
+func batchCompile(be *batchEvaler) {
+	if be.compiled {
 		return
 	}
 	// b.policySet = cedar.NewPolicySet() // TODO: pre-set size?
 	// for _, p := range b.policies {
 	// 	b.policySet.Store(p.PolicyID, cedar.NewPolicyFromAST((*publicast.Policy)(p.Policy)))
 	// }
-	b.evalers = make([]*idEvaler, len(b.policies))
-	for i, p := range b.policies {
-		b.evalers[i] = &idEvaler{PolicyID: p.PolicyID, Evaler: eval.Compile(p.Policy), Effect: types.Effect(p.Policy.Effect), Position: types.Position(p.Policy.Position)}
+	be.evalers = make([]*idEvaler, len(be.policies))
+	for i, p := range be.policies {
+		be.evalers[i] = &idEvaler{PolicyID: p.PolicyID, Evaler: eval.Compile(p.Policy), Effect: types.Effect(p.Policy.Effect), Position: types.Position(p.Policy.Position)}
 	}
-	b.compiled = true
+	be.compiled = true
 }
 
 // cloneSub will return a new value if any of its children have changed
