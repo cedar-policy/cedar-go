30fa19b76c1b04bd971a01b9652c851bd8d2ddf3
philhassey <phil@strongdm.com>
2024-09-11 09:20:08 -0600

    x/exp/batch: improve docs for ignore
    Addresses IDX-50
    
    Signed-off-by: philhassey <phil@strongdm.com>

--- x/exp/batch/batch.go
+++ x/exp/batch/batch.go
@@ -14,6 +14,7 @@ import (
 )
 
 // Ignore returns a value that should be ignored during batch evaluation.
+// The documentation for [WithIgnorePermit] and [WithIgnoreForbid] explain the exact behavior.
 func Ignore() types.Value { return eval.Ignore() }
 
 // Variable returns a named variable that is populated during batch evaluation.
