be5fd82750e99a0ad8eb146da39b56652b452e86
Patrick Jakubowski <patrick.jakubowski@strongdm.com>
2025-06-18 16:18:03 -0700

    types: add Entity.Equal() implementation
    Having an implementation of Equal() adds a canonical method of checking for Entity equality and makes it compatible with the cmp package's [Equal()](https://pkg.go.dev/github.com/google/go-cmp/cmp#Equal) function and other types in this package (e.g. Record, EntityUID).
    
    Signed-off-by: Patrick Jakubowski <patrick.jakubowski@strongdm.com>

--- types/entity_test.go
+++ types/entity_test.go
@@ -94,3 +94,102 @@ func TestEntityTagMarshalJSON(t *testing.T) {
             }
 		}`)
 }
+
+func TestEntityEqual(t *testing.T) {
+	t.Parallel()
+
+	baseEntity := types.Entity{
+		UID: types.NewEntityUID("FooType", "1"),
+		Parents: types.NewEntityUIDSet(
+			types.NewEntityUID("BarType", "1"),
+			types.NewEntityUID("BazType", "2"),
+		),
+		Attributes: types.NewRecord(types.RecordMap{
+			"attr1": types.String("value1"),
+			"attr2": types.Long(42),
+		}),
+		Tags: types.NewRecord(types.RecordMap{
+			"tag1": types.String("tagvalue1"),
+		}),
+	}
+
+	tests := []struct {
+		name   string
+		entity types.Entity
+		other  types.Entity
+		want   bool
+	}{
+		{
+			name:   "same content",
+			entity: baseEntity,
+			other:  baseEntity,
+			want:   true,
+		},
+		{
+			name:   "different UID",
+			entity: baseEntity,
+			other: types.Entity{
+				UID:        types.NewEntityUID("FooType", "2"),
+				Parents:    baseEntity.Parents,
+				Attributes: baseEntity.Attributes,
+				Tags:       baseEntity.Tags,
+			},
+			want: false,
+		},
+		{
+			name:   "different parents",
+			entity: baseEntity,
+			other: types.Entity{
+				UID: baseEntity.UID,
+				Parents: types.NewEntityUIDSet(
+					types.NewEntityUID("BarType", "1"),
+					types.NewEntityUID("DifferentType", "2"),
+				),
+				Attributes: baseEntity.Attributes,
+				Tags:       baseEntity.Tags,
+			},
+			want: false,
+		},
+		{
+			name:   "different attributes",
+			entity: baseEntity,
+			other: types.Entity{
+				UID:     baseEntity.UID,
+				Parents: baseEntity.Parents,
+				Attributes: types.NewRecord(types.RecordMap{
+					"attr1": types.String("different_value"),
+					"attr2": types.Long(42),
+				}),
+				Tags: baseEntity.Tags,
+			},
+			want: false,
+		},
+		{
+			name:   "different tags",
+			entity: baseEntity,
+			other: types.Entity{
+				UID:        baseEntity.UID,
+				Parents:    baseEntity.Parents,
+				Attributes: baseEntity.Attributes,
+				Tags: types.NewRecord(types.RecordMap{
+					"tag1": types.String("different_tagvalue"),
+				}),
+			},
+			want: false,
+		},
+		{
+			name:   "empty entities",
+			entity: types.Entity{},
+			other:  types.Entity{},
+			want:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			testutil.Equals(t, tt.entity.Equal(tt.other), tt.want)
+		})
+	}
+}
