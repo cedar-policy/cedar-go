7ca921b2e478e7010d60411e81f4791edb9da6c5
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-09-26 13:04:22 +0200

    feat: add decoder to read policies from an io.Reader
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- stream.go
+++ stream.go
@@ -3,6 +3,7 @@ package cedar
 import (
 	"io"
 
+	"github.com/cedar-policy/cedar-go/ast"
 	"github.com/cedar-policy/cedar-go/internal/parser"
 )
 
@@ -22,4 +23,31 @@ func NewEncoder(w io.Writer) *Encoder {
 // Encode encodes and writes a single [Policy] statement to the underlying [io.Writer].
 func (e *Encoder) Encode(p *Policy) error {
 	return e.enc.Encode((*parser.Policy)(p.AST()))
+}
+
+// Decoder reads, parses and compiles [Policy] statements in the human-readable format specified by the [Cedar documentation]
+// from an [io.Reader].
+//
+// [Cedar documentation]: https://docs.cedarpolicy.com/policies/syntax-grammar.html
+type Decoder struct {
+	dec *parser.Decoder
+}
+
+// NewDecoder returns a new [Decoder].
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{dec: parser.NewDecoder(r)}
+}
+
+// Decode parses and compiles a single [Policy] statement from the underlying [io.Reader].
+func (e *Decoder) Decode(p *Policy) error {
+	var parserPolicy parser.Policy
+
+	err := e.dec.Decode(&parserPolicy)
+	if err != nil {
+		return err
+	}
+
+	*p = *NewPolicyFromAST((*ast.Policy)(&parserPolicy))
+
+	return nil
 }
--- stream_test.go
+++ stream_test.go
@@ -3,10 +3,13 @@ package cedar_test
 import (
 	"bytes"
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/cedar-policy/cedar-go"
+	"github.com/cedar-policy/cedar-go/ast"
 	"github.com/cedar-policy/cedar-go/internal/testutil"
+	internalast "github.com/cedar-policy/cedar-go/x/exp/ast"
 )
 
 func newPolicy() *cedar.Policy {
@@ -66,10 +69,45 @@ func TestEncoderError(t *testing.T) {
 	policy := newPolicy()
 
 	_, w := io.Pipe()
-	w.Close()
+	_ = w.Close()
 
 	encoder := cedar.NewEncoder(w)
 
 	err := encoder.Encode(policy)
 	testutil.Error(t, err)
+}
+
+func TestDecoder(t *testing.T) {
+	policyStr := `permit (
+		principal,
+		action,
+		resource
+	);
+	forbid (
+		principal,
+		action,
+		resource
+	);
+
+`
+
+	decoder := cedar.NewDecoder(strings.NewReader(policyStr))
+
+	var actualPolicy0 cedar.Policy
+	testutil.OK(t, decoder.Decode(&actualPolicy0))
+
+	astPolicy0 := internalast.Permit()
+	astPolicy0.Position = internalast.Position{Offset: 0, Line: 1, Column: 1}
+	expectedPolicy0 := cedar.NewPolicyFromAST((*ast.Policy)(astPolicy0))
+	testutil.Equals(t, &actualPolicy0, expectedPolicy0)
+
+	var actualPolicy1 cedar.Policy
+	testutil.OK(t, decoder.Decode(&actualPolicy1))
+
+	astPolicy1 := internalast.Forbid()
+	astPolicy1.Position = internalast.Position{Offset: 48, Line: 6, Column: 2}
+	expectedPolicy1 := cedar.NewPolicyFromAST((*ast.Policy)(astPolicy1))
+	testutil.Equals(t, &actualPolicy1, expectedPolicy1)
+
+	testutil.ErrorIs(t, decoder.Decode(nil), io.EOF)
 }
